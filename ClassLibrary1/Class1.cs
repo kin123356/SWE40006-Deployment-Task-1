4362368e518db7df77b25579f63f64bb3e1946aa
kin123356 <khanhhuyenn035@gmail.com>
2025-08-24 17:06:33 +1000

    Added project

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        ClassLibrary2.MathEngine mathEngine = new ClassLibrary2.MathEngine();
+
+        public int Calculate(int operand1, int operand2, string operation)
+        {
+            switch(operation)
+            {
+                case "+": return mathEngine.Add(operand1, operand2);
+                case "-": return mathEngine.Subtract(operand1, operand2);
+                case "*": return mathEngine.Multiply(operand1, operand2);
+                case "/": return mathEngine.Divide(operand1, operand2);
+                default: throw new NotImplementedException();
+            }
+
+        }
+    }
+}
---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class MathEngine
+    {
+        public int Add(int a, int b)
+            { return a + b; }
+
+        public int Subtract(int a, int b) 
+            { return a - b; }
+
+        public int Multiply(int a, int b)
+            { return a * b; }
+
+        public int Divide(int a, int b)
+            { return a / b; }
+    }
+}
